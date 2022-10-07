5f91029a46f468a369d61c86b7461a66d86da05f
Guangxuan-Xiao <xgx@mit.edu>
2022-10-07 18:41:13 +0000

    transa transb bug fixed

--- tests/test_gemm.py
+++ tests/test_gemm.py
@@ -3,18 +3,28 @@ import torch
 from torch_int import gemm
 
 def test_gemm():
-    a = torch.randn(4, 4)
-    b = torch.randn(4, 4)
-    print(a)
-    print(b)
-    c_gt = torch.mm(a, b)
-    print(c_gt)
+    a = torch.randint(-128, 127, (4, 8), dtype=torch.int8)
+    b = torch.randint(-128, 127, (8, 12), dtype=torch.int8)
 
     a = a.to(torch.int8).cuda()
     b = b.to(torch.int8).cuda()
     print(a)
     print(b)
-    c = gemm(a, b)
+    transa, transb = False, False
+    c = gemm(a, b, transa, transb)
+    print(c)
+    print(torch.allclose(c.float(), torch.mm(a.float(), b.float())))
+
+    transa, transb = True, False
+    c = gemm(a.T, b, transa, transb)
+    print(c)
+
+    transa, transb = False, True
+    c = gemm(a, b.T, transa, transb)
+    print(c)
+
+    transa, transb = True, True
+    c = gemm(a.T, b.T, transa, transb)
     print(c)
 
 if __name__ == '__main__':
--- torch_int/gemm.cu
+++ torch_int/gemm.cu
@@ -6,19 +6,13 @@
 #include <torch/torch.h>
 
 
-torch::Tensor igemm(torch::Tensor A, torch::Tensor B)
+torch::Tensor gemm(torch::Tensor A, torch::Tensor B, bool transa, bool transb)
  {
     // Input: A, B are 2D signed 8-bit integer tensors
     // Output: C is 2D signed 32-bit integer tensor
-    // B is transposed
     
     const int alpha = 1;
     const int beta = 0; // C = alpha * A * B + beta * C
-
-
-    int m = A.size(0);
-    int k = A.size(1);
-    int n = B.size(0);
     
     cublasStatus_t stat;
     
@@ -26,20 +20,32 @@ torch::Tensor igemm(torch::Tensor A, torch::Tensor B)
     cudaDataType_t Btype = CUDA_R_8I;
     cudaDataType_t Ctype = CUDA_R_32I;
     cublasComputeType_t computeType = CUBLAS_COMPUTE_32I;
-    cublasGemmAlgo_t algo = CUBLAS_GEMM_DEFAULT;
+    cublasGemmAlgo_t algo = CUBLAS_GEMM_DEFAULT_TENSOR_OP;
 
     cublasHandle_t handle;
 
     stat = cublasCreate(&handle);
     if (stat != CUBLAS_STATUS_SUCCESS) {
-        std::cout << "CUBLAS initialization failed" << std::endl;
+        throw std::runtime_error("CUBLAS initialization failed");
     }
     
-    // cublasSetMathMode(handle, CUBLAS_TENSOR_OP_MATH);
-    
-    torch::Tensor C = torch::zeros({m, n}, torch::dtype(torch::kInt).device(torch::kCUDA));
+    torch::Device device = A.device();
 
-    stat = cublasGemmEx(handle, CUBLAS_OP_N, CUBLAS_OP_T, m, n, k, &alpha, A.data_ptr(), Atype, m, B.data_ptr(), Btype, k, &beta, C.data_ptr(), Ctype, m, computeType, algo);
+    cublasOperation_t transa_ = transa ? CUBLAS_OP_T : CUBLAS_OP_N;
+    cublasOperation_t transb_ = transb ? CUBLAS_OP_T : CUBLAS_OP_N;
+
+    int n = transa ? A.size(1) : A.size(0);
+    int k = transa ? A.size(0) : A.size(1);
+    int m = transb ? B.size(0) : B.size(1);
+
+    int lda = transa ? n : k;
+    int ldb = transb ? k : m;
+    int ldc = m;
+
+    torch::Tensor C = torch::zeros({n, m}, torch::dtype(torch::kInt).device(device));
+
+    // Note that CuBLAS assumes column-major matrices, so we actually pass the transposes of A and B    
+    stat = cublasGemmEx(handle, transb_, transa_, m, n, k, &alpha, B.data_ptr(), Btype, ldb, A.data_ptr(), Atype, lda, &beta, C.data_ptr(), Ctype, ldc, computeType, algo);
 
     if (stat != CUBLAS_STATUS_SUCCESS) {
         throw std::runtime_error("CUBLAS GEMM failed, error code: " + std::string(cublasGetStatusString(stat)));
@@ -50,5 +56,5 @@ torch::Tensor igemm(torch::Tensor A, torch::Tensor B)
 
 
 PYBIND11_MODULE(TORCH_EXTENSION_NAME, m) {
-    m.def("igemm", &igemm, "igemm (CUDA)");
+    m.def("gemm", &gemm, "GEMM (CUDA)");
 }
