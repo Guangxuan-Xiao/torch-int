39c77fc031118734fc87b2447fc00daa9ed04fde
Guangxuan-Xiao <xgx@mit.edu>
2022-10-10 17:32:33 +0000

    define quantizer api

--- torch_int/functional/quantization.py
+++ torch_int/functional/quantization.py
@@ -1,14 +1,28 @@
 import torch
 
-def quantize_per_tensor_min_max(t, use_zero_point=False):
+
+@torch.no_grad()
+def dynamic_quantize_per_tensor_min_max(t, use_zero_point=False):
     min, max = torch.min(t), torch.max(t)
 
 
-def quantize_per_channel_min_max(t, axis, use_zero_point=False):
+@torch.no_grad()
+def dynamic_quantize_per_channel_min_max(t, axis, use_zero_point=False):
     pass
 
-def dequantize_per_tensor_min_max(t, scale, zero_point=0):
+@torch.no_grad()
+def dequantize_per_tensor(t, scale, zero_point=0):
     pass
 
-def dequantize_per_channel_min_max(t, axis, scale, zero_point=0):
+@torch.no_grad()
+def dequantize_per_channel(t, axis, scale, zero_point=0):
+    pass
+
+@torch.no_grad()
+def static_quantize_per_tensor(t, scale, zero_point=0):
+    pass
+
+
+@torch.no_grad()
+def static_quantize_per_channel(t, axis, scale, zero_point=0):
     pass
