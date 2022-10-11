f6c251c92d05ddb0bf4c3494a6e4c9ce7c36ea4c
Guangxuan-Xiao <xgx@mit.edu>
2022-10-11 02:24:22 +0000

    register buffer instead of tensor to make all in one device

--- torch_int/nn/linear.py
+++ torch_int/nn/linear.py
@@ -8,19 +8,22 @@ from ..functional.quantization import (
     dequantize_activation_w_per_channel_a_per_tensor,
 )
 
+
 class Int8Linear(torch.nn.Module):
     def __init__(self, in_features, out_features, bias=True, act_quant='per_token'):
         super(Int8Linear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
-        self.weight = torch.randint(-127, 127, (self.out_features,
-                                    self.in_features), dtype=torch.int8)
+
+        self.register_buffer('weight', torch.randint(-127, 127, (self.out_features,
+                                                                 self.in_features), dtype=torch.int8, requires_grad=False))
         if bias:
-            self.bias = torch.zeros(
-                (1, self.out_features), dtype=torch.float16)
+            self.register_buffer('bias', torch.zeros(
+                (1, self.out_features), dtype=torch.float16, requires_grad=False))
         else:
-            self.register_parameter('bias', None)
-        self.weight_scales = torch.ones(self.out_features, dtype=torch.float16)
+            self.register_buffer('bias', None)
+        self.register_buffer('weight_scales', torch.ones(
+            self.out_features, dtype=torch.float16, requires_grad=False))
         if act_quant == 'per_token':
             self.activation_quantizer = dynamic_quantize_activation_per_token_min_max
             self.activation_dequantizer = dequantize_activation_w_per_channel_a_per_token
@@ -55,7 +58,8 @@ class Int8Linear(torch.nn.Module):
         assert isinstance(module, torch.nn.Linear)
         new_module = Int8Linear(
             module.in_features, module.out_features, module.bias is not None, act_quant)
-        new_module.weight, new_module.weight_scales = quantize_weight_per_channel_min_max(module.weight)
+        new_module.weight, new_module.weight_scales = quantize_weight_per_channel_min_max(
+            module.weight)
         if module.bias is not None:
             new_module.bias = module.bias.to(torch.float16)
         return new_module
