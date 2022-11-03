14de29e4e0d23076885f251521f3479b3a2837be
Guangxuan-Xiao <xgx@mit.edu>
2022-11-03 06:38:31 +0000

    bmm test passed

--- tests/test_bmm.py
+++ tests/test_bmm.py
@@ -5,18 +5,14 @@ from icecream import ic
 
 @torch.no_grad()
 def test_bmm():
-    B, M, N = 2, 16, 32
+    B, M, N = 8, 512, 12288
     a = torch.randint(-128, 127, (B, M, N), dtype=torch.int8)
-    ic(a)
     b = torch.randint(-128, 127, (B, M, N), dtype=torch.int8)
-    ic(b)
     scale = 0.01
     c_gt = torch.bmm(a.float(), b.float().transpose(1, 2)) * scale
     c_gt = c_gt.clamp(-128, 127).round()
-    ic(c_gt)
     c = bmm_s8t_s8n_s8t(a.cuda(), b.cuda(), scale)
-    ic(c)
-    ic(torch.allclose(c_gt, c.float().cpu(), atol=1))
+    ic(torch.allclose(c_gt, c.float().cpu(), atol=1e-3))
 
 
 if __name__ == '__main__':
