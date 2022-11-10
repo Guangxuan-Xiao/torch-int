e144f4fe9d0e5dc5826b6b5f1b281087cd797f9b
Guangxuan-Xiao <xgx@mit.edu>
2022-11-10 20:21:31 +0000

    ablate quantization schemes

--- benchmark/bench_opt.py
+++ benchmark/bench_opt.py
@@ -1,8 +1,12 @@
 import torch
 from torch_int.nn.opt import Int8OPTDecoder, Int8OPTForCausalLM, Int8OPTModel
 from transformers.models.opt.modeling_opt import OPTDecoder, OPTConfig, OPTForCausalLM, OPTModel
+from torch_int.nn.linear import W8A8BFP32OFP32Linear, W8A8B8O8Linear, W8A8B8O8LinearReLU
+from torch_int.nn.bmm import BMM_S8T_S8N_S8T, BMM_S8T_S8N_F32T
+
 from utils import bench_model
 import argparse
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -33,6 +37,45 @@ if __name__ == '__main__':
     elif args.precision == 'llm_int8':
         model = OPTModel.from_pretrained(
             model_path, device_map='auto', load_in_8bit=True)
+    elif args.precision == 'llm_int8_0':
+        model = OPTModel.from_pretrained(
+            model_path, device_map='auto', load_in_8bit=True)
+        from bitsandbytes.nn import Linear8bitLt
+        for name, module in model.named_modules():
+            if isinstance(module, Linear8bitLt):
+                module.state.threshold = 0
+    elif args.precision == 'int8-fp16-dynamic-a-token':
+        model_path += '-int8'
+        model = Int8OPTModel.from_pretrained(
+            model_path, device_map='auto', torch_dtype=torch.half)
+        for name, module in model.named_modules():
+            if isinstance(module, (W8A8BFP32OFP32Linear, W8A8B8O8Linear, W8A8B8O8LinearReLU)):
+                module.old_forward = module.forward
+
+                def get_new_forward(self):
+                    def new_forward(input):
+                        absmax = input.abs().max(dim=-1, keepdim=True)[0]
+                        output = self.old_forward(input)
+                        absmax = output.abs().max(dim=-1, keepdim=True)[0]
+                        return output
+                    return new_forward
+                module.forward = get_new_forward(module)
+    elif args.precision == 'int8-fp16-dynamic-a-tensor':
+        model_path += '-int8'
+        model = Int8OPTModel.from_pretrained(
+            model_path, device_map='auto', torch_dtype=torch.half)
+        for name, module in model.named_modules():
+            if isinstance(module, (W8A8BFP32OFP32Linear, W8A8B8O8Linear, W8A8B8O8LinearReLU)):
+                module.old_forward = module.forward
+
+                def get_new_forward(self):
+                    def new_forward(input):
+                        absmax = input.abs().max()
+                        output = self.old_forward(input)
+                        absmax = output.abs().max()
+                        return output
+                    return new_forward
+                module.forward = get_new_forward(module)
     else:
         raise NotImplementedError
     bench_model(model, (input_ids, ), num_iter=500)
---
+++ scripts/ablate_dynamic_quant.sh
@@ -0,0 +1,20 @@
+models="opt-13b opt-30b"
+precisions="llm_int8 llm_int8_0 fp16 int8-fp16 int8-fp16-dynamic-a-token int8-fp16-dynamic-a-tensor"
+batch_sizes="4"
+seq_lens="256 512"
+output_file=log/ablate_quantization_scheme.csv
+touch $output_file
+
+echo "model,precision,batch_size,seq_len,latency(ms),memory(MB)" >$output_file
+for seq_len in $seq_lens; do
+    for batch_size in $batch_sizes; do
+        for model in $models; do
+            for precision in $precisions; do
+                output=$(CUDA_VISIBLE_DEVICES=5 python benchmark/bench_opt.py --model $model --precision $precision --seq-len $seq_len --batch-size $batch_size)
+                ms=$(echo "$output" | grep "Average inference time:" | awk '{print $4}')
+                mem=$(echo "$output" | grep "Peak memory usage:" | awk '{print $4}')
+                echo "$model,$precision,$batch_size,$seq_len,$ms,$mem" | tee -a $output_file
+            done
+        done
+    done
+done
