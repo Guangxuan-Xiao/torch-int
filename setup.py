778eff0323d37ee0e0bb1f330d9e28a174df6591
Guangxuan-Xiao <xgx@mit.edu>
2022-11-29 17:42:47 +0000

    remove opt configs

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setup(
             include_dirs=['torch_int/kernels/include'],
             extra_link_args=['-lcublas_static', '-lcublasLt_static',
                              '-lculibos', '-lcudart', '-lcudart_static',
-                             '-lrt', '-lpthread', '-ldl'],
+                             '-lrt', '-lpthread', '-ldl', '-L/usr/lib/x86_64-linux-gnu/'],
             extra_compile_args={'cxx': ['-std=c++14', '-O3'],
                                 'nvcc': ['-O3', '-std=c++14', '-U__CUDA_NO_HALF_OPERATORS__', '-U__CUDA_NO_HALF_CONVERSIONS__', '-U__CUDA_NO_HALF2_OPERATORS__']},
         ),
