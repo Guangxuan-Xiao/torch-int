45a1596aec97381f83ff245c01e2eb2790e03c26
Iman Hosseini <hosseini.iman@yahoo.com>
2022-12-05 22:42:14 -0500

    adding LinearGELU kernel

--- torch_int/kernels/include/linear.h
+++ torch_int/kernels/include/linear.h
@@ -32,6 +32,14 @@ torch::Tensor linear_relu_a8_w8_b8_o8(torch::Tensor input,  // INT8
                                       float beta            // FP32
 );
 
+// used by fc1, return INT8
+torch::Tensor linear_gelu_a8_w8_b8_o8(torch::Tensor input,  // INT8
+                                      torch::Tensor weight, // INT8
+                                      torch::Tensor bias,   // INT8
+                                      float alpha,          // FP32
+                                      float beta            // FP32
+);
+
 // used by q_proj, k_proj, v_proj, return INT8
 torch::Tensor linear_a8_w8_b8_o8(torch::Tensor input,  // INT8
                                  torch::Tensor weight, // INT8
