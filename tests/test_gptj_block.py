e7ad7ad8357bd33a7d41fb47bfcdcb25874fe4d0
Iman Hosseini <hosseini.iman@yahoo.com>
2022-12-07 12:24:15 -0500

    fix

--- tests/test_gptj_block.py
+++ tests/test_gptj_block.py
@@ -20,7 +20,7 @@ def store_act(module, x, y, act_dict, name):
 def test_gptj_block():
     config : GPTJConfig = GPTJConfig.from_pretrained('Salesforce/codegen-350M-mono')
     B, L, D, H = 1, 256, config.n_embd, config.n_head
-    x = torch.randn(B, L, D)*20
+    x = torch.randn(B, L, D)*10
     blk = GPTJBlock(config)
     blk.eval()
     act_dict = {}
@@ -45,34 +45,33 @@ def test_gptj_block():
     fc1_input_scale = act_dict['mlp.fc_in'][0].abs().max() / 127
     fc2_input_scale = act_dict['mlp.fc_out'][0].abs().max() / 127
     int8_blk = Int8GPTJBlock.from_float(
-        blk, attn_input_scale, q_output_scale, k_output_scale, v_output_scale, out_input_scale, fc1_input_scale, fc2_input_scale,
-        ln1_input_scale).cuda()
+        blk, attn_input_scale, q_output_scale, k_output_scale, v_output_scale, out_input_scale, fc1_input_scale, fc2_input_scale).cuda()
     int8_blk.eval()
     q_act_dict = {}
 
     y_hat = int8_blk(x.cuda())[0].cpu()
-    rd = blk.dbgi
-    md = int8_blk.dbgi
-    RN = 256
-    ra = rd['atto'].cpu().flatten()[:RN]
-    ma = md['attoX'].cpu().flatten()[:RN]
-    rf = rd['ffn'].cpu().flatten()[:RN]
-    mf = md['ffnX'].cpu().flatten()[:RN]
-    rr = rd['resi'].cpu().flatten()[:RN]
-    mr = md['resiX'].cpu().flatten()[:RN]
+    # rd = blk.dbgi
+    # md = int8_blk.dbgi
+    # RN = 256
+    # ra = rd['atto'].cpu().flatten()[:RN]
+    # ma = md['attoX'].cpu().flatten()[:RN]
+    # rf = rd['ffn'].cpu().flatten()[:RN]
+    # mf = md['ffnX'].cpu().flatten()[:RN]
+    # rr = rd['resi'].cpu().flatten()[:RN]
+    # mr = md['resiX'].cpu().flatten()[:RN]
     #
     # plt.plot(ra.flatten())
-    print(f"MAX: a:{ra.abs().max()} f:{rf.abs().max()} r:{rr.abs().max()+0.0000001}")
-    plt.plot(ma - ra, color='r')
-    plt.savefig("Xa.jpg", dpi=300)
-    plt.cla()
-    # plt.plot(rf)
-    plt.plot(mf - rf, color='r')
-    plt.savefig("Xf.jpg", dpi=300)
-    plt.cla()
-    # plt.plot(rr.flatten())
-    plt.plot(mr - rr, color='r')
-    plt.savefig("Xr.jpg", dpi=300)
+    # print(f"MAX: a:{ra.abs().max()} f:{rf.abs().max()} r:{rr.abs().max()+0.0000001}")
+    # plt.plot(ma - ra, color='r')
+    # plt.savefig("Xa.jpg", dpi=300)
+    # plt.cla()
+    # # plt.plot(rf)
+    # plt.plot(mf - rf, color='r')
+    # plt.savefig("Xf.jpg", dpi=300)
+    # plt.cla()
+    # # plt.plot(rr.flatten())
+    # plt.plot(mr - rr, color='r')
+    # plt.savefig("Xr.jpg", dpi=300)
 
     r2 = (y - y_hat).pow(2).mean() / y.pow(2).mean()
     ic(r2)
